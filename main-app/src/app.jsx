af9a36087488887fa43915f9bd31138909a7d146
bota.lb <bota.lb@alibaba-inc.com>
2020-10-05 00:30:37 +0800

    框架应用添加样式前缀

--- main-app/src/app.jsx
+++ main-app/src/app.jsx
@@ -1,11 +1,16 @@
+import React from 'react'
 import { createApp } from 'ice';
+import { ConfigProvider } from '@alifd/next'
 import './global.scss'
 const appConfig = {
   app: {
     rootId: 'ice-container',
+    addProvider: ({ children }) => (
+      <ConfigProvider prefix="next-layout-">{children}</ConfigProvider>
+    )
   },
   router: {
-    type: 'browser',
+    type: 'browser'
   }
-};
-createApp(appConfig);
+}
+createApp(appConfig)
