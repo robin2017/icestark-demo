9db143b24af870ccff81dc68daa42866ae5a0cca
bota.lb <bota.lb@alibaba-inc.com>
2020-10-08 00:41:38 +0800

    官方示例跳转体验很好！！！

--- demo-office/main-app/src/app.tsx
+++ demo-office/main-app/src/app.tsx
@@ -22,33 +22,20 @@ const appConfig: IAppConfig = {
         path: '/seller',
         title: '商家平台',
         sandbox: true,
-        // React app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-seller-react-16
-        // url: [
-        //   '//ice.alicdn.com/icestark/child-seller-react/index.js',
-        //   '//ice.alicdn.com/icestark/child-seller-react/index.css',
-        // ],
         url:[
           'http://localhost:3101/js/index.js',
           'http://localhost:3101/css/index.css',
         ]
       }, 
-      // {
-      //   path: '/waiter',
-      //   title: '小二平台',
-      //   sandbox: true,
-      //   url: [
-      //     // Vue app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-waiter-vue-2
-      //     '//ice.alicdn.com/icestark/child-waiter-vue/app.js',
-      //     '//ice.alicdn.com/icestark/child-waiter-vue/app.css'
-      //   ],
-      // }, 
-      // {
-      //   path: '/angular',
-      //   title: 'Angular',
-      //   sandbox: true,
-      //   // Angular app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-common-angular-9 
-      //   entry: '//ice.alicdn.com/icestark/child-common-angular/index.html',
-      // }
+      {
+        path: '/waiter',
+        title: '小二平台',
+        sandbox: true,
+        url: [
+          'http://localhost:3102/js/index.js',
+          'http://localhost:3102/css/index.css',
+        ],
+      }
     ];
       return apps;
     },
--- demo-office/main-app/src/layouts/BasicLayout/menuConfig.ts
+++ demo-office/main-app/src/layouts/BasicLayout/menuConfig.ts
@@ -16,64 +16,44 @@ const asideMenuConfig = [
     path: '/login',
     icon: 'account',
   },
-  // {
-  //   name: 'Angular',
-  //   icon: 'set',
-  //   children: [
-  //     {
-  //       path: '/angular',
-  //       name: 'router contact',
-  //     },
-  //     {
-  //       path: '/angular/detail',
-  //       name: 'router detail',
-  //     },
-  //   ]
-  // },
   {
     name: '商家平台',
     icon: 'atm',
-    children: [
-      {
-        path: '/seller',
-        name: '商家首页',
-      },
-      // {
-      //   path: '/seller/list',
-      //   name: '商家列表',
-      // },
-      {
-        path: '/seller/detail',
-        name: '商家详情',
-      },
-      // {
-      //   path: '/seller/404',
-      //   name: '商家 404',
-      // },
-    ],
+    path:'/seller',
+    // children: [
+    //   {
+    //     path: '/seller',
+    //     name: '商家首页',
+    //   },
+    //   {
+    //     path: '/seller/detail',
+    //     name: '商家详情',
+    //   },
+    // ],
   },
-  // {
-  //   name: '小二平台',
-  //   icon: 'account',
-  //   children: [
-  //     {
-  //       path: '/waiter',
-  //       name: '小二首页',
-  //     },
-  //     {
-  //       path: '/waiter/list',
-  //       name: '小二列表',
-  //     },
-  //     {
-  //       path: '/waiter/detail',
-  //       name: '小二详情',
-  //     },
-  //     {
-  //       path: '/waiter/404',
-  //       name: '小二 404',
-  //     },
-  //   ],
-  // },
+  {
+    name: '小二平台',
+    icon: 'account',
+    path:'/waiter' //这里路径和子应用没有任何关系？？？！！！
+    // children: [
+    //   {
+    //     path: '/waiter',
+    //     name: '小二首页',
+    //   },
+    //   {
+    //     path: '/waiter/list',
+    //     name: '小二列表',
+    //   },
+    //   {
+    //     path: '/waiter/detail',
+    //     name: '小二详情',
+    //   },
+    //   {
+    //     path: '/waiter/404',
+    //     name: '小二 404',
+    //   },
+    // ],
+  },
 ];
 
 export { headerMenuConfig, asideMenuConfig };
